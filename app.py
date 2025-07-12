1687f13b64ceacb866ef0f081beee60e259da756
Wellinton Oliveira <wellintonoliveira04@gmail.com>
2025-07-12 10:41:21 -0300

    add: setup github action CI code

--- app.py
+++ app.py
@@ -82,3 +82,6 @@ async def predict_route(request: Request, file: UploadFile = File(...)):
         return templates.TemplateResponse('table.html', {'request': request, 'table': table_html})
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+if __name__ == '__main__':
+    app_run(app, host='0.0.0.0', port=8000)
